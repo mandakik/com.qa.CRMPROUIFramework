49f874d061073185e42d16f5acf8dca6f9994f97
Mandakini <kalemandakini149@gmail.com>
2026-01-08 16:28:32 +0530

    added Extent report

---
+++ src/main/java/Listeners/ExtentReporterNG.java
@@ -0,0 +1,75 @@
+package Listeners;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ExtentReporterNG implements IReporter {
+
+    private ExtentReports extent;
+
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
+                               String outputDirectory) {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(outputDirectory + File.separator + "Extent.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+
+        for (ISuite suite : suites) {
+            Map<String, ISuiteResult> result = suite.getResults();
+
+            for (ISuiteResult r : result.values()) {
+                ITestContext context = r.getTestContext();
+
+                buildTestNodes(context.getPassedTests(), Status.PASS);
+                buildTestNodes(context.getFailedTests(), Status.FAIL);
+                buildTestNodes(context.getSkippedTests(), Status.SKIP);
+            }
+        }
+
+        extent.flush();
+        //extent.close();
+    }
+
+    private void buildTestNodes(IResultMap tests, Status status) {
+        ExtentTest test;
+
+        if (tests.size() > 0) {
+            for (ITestResult result : tests.getAllResults()) {
+                test = extent.createTest(result.getMethod().getMethodName());
+
+
+                test.getModel().setStartTime(getTime(result.getStartMillis()));
+                test.getModel().setEndTime(getTime(result.getEndMillis()));
+
+                for (String group : result.getMethod().getGroups())
+                    test.assignCategory(group);
+
+                if (result.getThrowable() != null) {
+                    test.log(Status.FAIL, result.getThrowable());
+
+                } else {
+                    test.log(status, "Test " + status.toString().toLowerCase()
+                            + "ed");
+                }
+
+                //extent.endTest(test);
+            }
+        }
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+}
